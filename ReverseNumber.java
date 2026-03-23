f46593a2899a3af5e836bfbb4ff554a9ee302a95
245123751059-cell <245123751059@mvsrec.edu.in>
2026-03-23 15:41:27 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,16 +1,13 @@
-class ReverseNumber
-{
-public static void main(String args[])
-{
-int n=Integer.parseInt(args[0]);
-int rev=0;
-int r;
-while(n>0)
-{
-r=n%10;
-rev=(rev*10)+r;
-n=n/10;
-}
-System.out.println("Reverse number:"+rev);
-}
+public class ReverseNumber {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number as an argument.");
+            return;
+        }
+
+        String input = args[0];
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        System.out.println("Reversed number: " + reversed);
+    }
 }
